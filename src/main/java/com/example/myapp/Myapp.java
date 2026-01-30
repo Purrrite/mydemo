abfade8bc995285df33af878086b683f02127932
Purrrite <deliciousjelly2@gmail.com>
2026-01-30 14:27:19 +0900

    omg i made hello world

--- src/main/java/com/example/myapp/Myapp.java
+++ src/main/java/com/example/myapp/Myapp.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Myapp {
@@ -9,5 +10,10 @@ public class Myapp {
 	public static void main(String[] args) {
 		SpringApplication.run(Myapp.class, args);
 	}
+
+	@GetMapping(value = "/")
+    public String hello() {
+        return "Hello World!";
+    }
 
 }
